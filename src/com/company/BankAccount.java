2a6042b0a90fc8e66b98067c235e13f0e1c05391
SchnitzelB <es971120@icloud.com>
2021-01-13 22:46:16 -0600

    Created instance variables and methods to handle transaction amount, balance, deposits and withdrawals.

--- src/com/company/BankAccount.java
+++ src/com/company/BankAccount.java
@@ -1,4 +1,47 @@
 package com.company;
 
 public class BankAccount {
+
+    private int accountNumber; //
+    private double actionAmount;
+    //int actionAmount handles any transaction amount throughout the program.
+    private double balance;
+    //int balance
+    private String customerName;
+    private String customerEmail;
+    private double phoneNumber;
+
+    /* The depositIntoBalance() method,
+    which acts as a setter, is used to
+    deposit funds into the user's account,
+    e.g. adding to the int balance.
+     */
+    public double depositIntoBalance(double actionAmount, double balance) {
+        // Makes the instance variable double balance == double balance from the parameter.
+        this.balance = balance;
+        // Makes the instance variable double actionAmount == double actionAmount from the parameter.
+        this.actionAmount = actionAmount;
+        // This adds actionAmount to balance
+        this.balance += this.actionAmount;
+        return this.balance;
+    }
+
+    //The getBalance() method is used to return the current balance to the user.
+    public double getBalance() {
+        return this.balance;
+    }
+
+    public double withdrawlFromBalance(double actionAmount, double balance) {
+        this.balance = balance;
+        this.actionAmount = actionAmount;
+        if (this.actionAmount > this.balance) {
+            System.out.println("INSUFFICIENT FUNDS ALERT");
+            return this.balance;
+        } else {
+            System.out.println("$" + this.actionAmount + " have been successfully deposited.");
+            this.balance -= this.actionAmount;
+            return this.balance;
+        }
+    }
+
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        BankAccount bankAccount = new BankAccount();
+
+        bankAccount.depositIntoBalance(200,5000);
+
+        System.out.println("Available funds: $" + bankAccount.getBalance());
+
+        bankAccount.withdrawlFromBalance(1400,5000);
+        System.out.println("Available funds: $" + bankAccount.getBalance());
+
+        bankAccount.withdrawlFromBalance(5400,5000);
+        System.out.println("Available funds: $" + bankAccount.getBalance());
     }
 }
